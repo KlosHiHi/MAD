767f7c21bb37c31df983d3e986a3549959c43abd
Вячеслав <slavvva05@gmail.com>
2026-03-11 10:12:22 +0300

    0311

--- lessons/lesson0310/app/src/main/java/com/example/lesson0310/MainActivity.kt
+++ lessons/lesson0310/app/src/main/java/com/example/lesson0310/MainActivity.kt
@@ -5,16 +5,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateColor
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.CubicBezierEasing
+import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.VectorConverter
+import androidx.compose.animation.core.animateDp
 import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.animateValue
+import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.keyframes
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.repeatable
 import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.updateTransition
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.awaitFirstDown
@@ -24,6 +39,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -31,12 +50,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.modifier.modifierLocalOf
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
@@ -53,7 +74,7 @@ class MainActivity : ComponentActivity() {
             Lesson0310Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Column(Modifier.padding(innerPadding)){
-                        AnimatableExample()
+                        InfiniteTransitionExample()
                     }
                 }
             }
@@ -162,4 +183,70 @@ fun AnimatableExample(){
             .background(Color.Black)
         )
     }
+}
+
+@Composable
+fun VisibilityAnimationExample(){
+    var isVisible by remember {mutableStateOf(true)}
+
+    Button({isVisible = !isVisible}) {
+        Text("Нажми")
+    }
+
+    AnimatedVisibility(
+        visible = isVisible,
+        enter = fadeIn(tween (1000)) + scaleIn(),
+        exit = fadeOut(tween (1000)) + scaleOut()
+    ){
+        Box(
+            modifier = Modifier
+                .size(200.dp)
+                .background(Color.Green)
+        )
+    }
+}
+
+@Composable
+fun UpdateTransitionExample(){
+    var isExpanded by remember {mutableStateOf(false)}
+
+    val transition = updateTransition(targetState = isExpanded)
+
+    val size by transition.animateDp { state ->
+        if(state) 150.dp else 100.dp
+    }
+
+    val color by transition.animateColor{ state ->
+        if(state) Color.Yellow else Color.LightGray
+    }
+
+    Box(
+        Modifier
+            .size(size)
+            .background(color)
+            .clickable{isExpanded = !isExpanded}
+    )
+}
+
+@Composable
+fun InfiniteTransitionExample(){
+    val transition = rememberInfiniteTransition()
+
+    val scale by transition.animateFloat(
+        initialValue = 4f, targetValue = 10f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(1000),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+
+    Box(
+        Modifier.fillMaxSize()){
+            Icon(imageVector = Icons.Filled.Favorite, contentDescription = "",
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .graphicsLayer(scaleX = scale, scaleY = scale),
+                tint = Color.Red)
+        }
+
 }
