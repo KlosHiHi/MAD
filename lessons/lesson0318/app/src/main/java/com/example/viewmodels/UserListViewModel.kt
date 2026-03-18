98dcc56f5ada326b11045d22412a3a02ac523682
Вячеслав <slavvva05@gmail.com>
2026-03-18 09:57:27 +0300

    1803

---
+++ lessons/lesson0318/app/src/main/java/com/example/viewmodels/UserListViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.viewmodels
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+data class User(val name:String, val age:Int)
+
+class UserListViewModel: ViewModel() {
+    val users = mutableStateListOf<User>()
+
+    var userName by mutableStateOf("")
+    var userAge by mutableStateOf(0)
+
+    fun addUser(){
+        users.add(User(userName, userAge))
+    }
+
+    fun deleteUser(user: User){
+        users.remove(user)
+    }
+
+    fun changeName(value: String){
+        userName = value
+    }
+
+    fun changeAge (value: String){
+        userAge = value.toIntOrNull()?:userAge
+    }
+
+
+}
