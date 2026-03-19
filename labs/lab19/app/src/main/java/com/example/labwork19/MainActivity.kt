e621d7e98c9bbb189e7b91c7a396f2a3bc03eba7
KlosHiHi <vaceslavvlasov420@gmail.com>
2026-03-20 00:30:24 +0300

    lab19

--- labs/lab19/app/src/main/java/com/example/labwork19/MainActivity.kt
+++ labs/lab19/app/src/main/java/com/example/labwork19/MainActivity.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -15,14 +18,29 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.pager.PageSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -32,7 +50,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.labwork19.ui.theme.LabWork19Theme
+import kotlinx.coroutines.launch
 
+data class Product( var article: String, var name : String,  var price : Float, var imageId : Int )
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -60,23 +80,82 @@ class MainActivity : ComponentActivity() {
                     Product("00018", "Чай", 500.56f, R.drawable.tea),
                     Product("00019", "Томаты", 270.84f, R.drawable.tomato),
                     Product("00020", "Вода", 49.49f, R.drawable.water),
-
                     )
+                
                 Scaffold() { innerPadding ->
-                    LazyColumn(Modifier.padding(innerPadding)) {
-                        items(products){
-                            product -> ProductCardOne(product)
-                            ProductCardTwo(product)
-                        }
-                    }
+//                    TaskTwo(modifier = Modifier.padding(innerPadding), products)
+//                    TaskThree(modifier = Modifier.padding(innerPadding), products)
+//                    ProductVerticalGrid(modifier = Modifier.padding(innerPadding), products)
+                    ProductHorizontalGrid(modifier = Modifier.padding(innerPadding), products)
                 }
             }
         }
     }
 }
 
+@Composable
+fun TaskTwo(modifier: Modifier, products: List<Product>) {
+    var text by remember { mutableStateOf("Продукт") }
+    val listState = rememberLazyListState()
+    val showButton = remember { derivedStateOf { listState.firstVisibleItemIndex > 0 } }
+    val coroutineScope = rememberCoroutineScope()
 
-data class Product( var article: String, var name : String,  var price : Float, var imageId : Int )
+    Box(modifier = Modifier.fillMaxSize()) {
+        LazyColumn(modifier, state = listState) {
+            item { Text(text, fontSize = 30.sp) }
+            items(products) { product ->
+                Box(Modifier.clickable {
+                    text = product.name
+                }) { ProductCardOne(product) }
+            }
+        }
+        AnimatedVisibility(
+            visible = showButton.value,
+            modifier = Modifier.align(Alignment.BottomEnd)
+        ) {
+            FloatingActionButton(
+                onClick = { coroutineScope.launch { listState.scrollToItem(index = 0) } },
+                modifier = Modifier.padding(10.dp, 30.dp),
+            ) {
+                Text("Назад")
+            }
+        }
+    }
+}
+
+@Composable
+fun TaskThree(modifier: Modifier, products: List<Product>) {
+    LazyRow(modifier) {
+        items(products) { product ->
+            ProductCardTwo(product)
+        }
+    }
+}
+
+@Composable
+fun ProductVerticalGrid(modifier: Modifier, products: List<Product>){
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        modifier = modifier,
+        horizontalArrangement = Arrangement.Center) {
+        items(products) { product ->
+            ProductCardTwo(product)
+        }
+   }
+}
+
+@Composable
+fun ProductHorizontalGrid(modifier: Modifier, products: List<Product>) {
+    LazyHorizontalGrid(
+        rows = GridCells.Fixed(3),
+        modifier = modifier,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        items(products) { product ->
+            ProductCardTwo(product)
+        }
+    }
+}
 
 @Composable
 fun ProductCardOne(product: Product) {
